6d6ab0b55b82ff44c871fe50ea4003ed4c51bdf6
Felix H. Dahlke <fhd@ubercode.de>
2011-01-16 17:25:58 +0100

    Made numbers up to 399 work and refactored.

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -5,7 +5,58 @@ import static org.hamcrest.CoreMatchers.*;
 
 
 public class RomanNumeralsTest {
-	
+
+    public static class RomanNumerals {
+
+    	enum Numeral{
+    		C(100),
+    		XC(90),
+    		L(50),
+    		XL(40),
+    		X(10),
+    		IX(9),
+    		V(5),
+    		IV(4),
+    		I(1);
+    		
+    		private int arab;
+    		
+    		private Numeral(int arab) {
+    		    this.arab = arab;
+    		}
+    	}
+    	
+        public static String convert(int number) {
+            if(number == 0) return "";
+        	for(Numeral numeral:Numeral.values()){
+        		int arab = numeral.arab;
+                if(number >= arab)
+        		    return numeral.toString() + convert(number - arab);
+        	}
+        	throw new IllegalStateException("Should not be able to get this far.");
+        }
+    }
+
+    @Test
+    public void threeHundredAndNinetyNine() {
+        assertThat("CCCXCIX", is(RomanNumerals.convert(399)));
+    }
+
+    @Test
+    public void ninety() {
+        assertThat("XC", is(RomanNumerals.convert(90)));
+    }
+
+    @Test
+    public void eightyNine() {
+        assertThat("LXXXIX", is(RomanNumerals.convert(89)));
+    }
+
+    @Test
+    public void sixty() {
+        assertThat("LX", is(RomanNumerals.convert(60)));
+    }
+
 	@Test
 	public void fifty() {
 		assertThat("L", is(RomanNumerals.convert(50)));
@@ -21,7 +72,6 @@ public class RomanNumeralsTest {
 		assertThat("XLI", is(RomanNumerals.convert(41)));
 	}
 	
-
 	@Test
 	public void one() {
 		assertThat("I", is(RomanNumerals.convert(1)));
